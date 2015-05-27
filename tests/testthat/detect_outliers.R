935a29fa8806a54e6fe94d83bb7214da2068f9c8
Rob J Hyndman <robjhyndman@gmail.com>
2015-05-27 15:39:31 +1000

    Corrected test code

--- tests/testthat/detect_outliers.R
+++ tests/testthat/detect_outliers.R
@@ -1,9 +1,9 @@
-context("anomaly tests")
+context("Anomalous time series tests")
 
-test_that("Time-series is constructed correctly, features are extracted properly and that anomaly method returns expected number of outliers.", {
+test_that("Time series is constructed correctly, features are extracted properly and that anomalous method returns expected number of outliers.", {
   z <- ts(matrix(rnorm(3000),ncol=100),freq=4)
-  expect_that(ncol(z) = 100, is_true())
+  expect_that(ncol(z) == 100, is_true())
   y <- tsmeasures(z)
-  expect_that(ncol(y) = 18, is_true())
-  expect_that(length(anomaly(y, n=2, method="hdr", plot=FALSE)) = 2, is_true()) 
+  expect_that(ncol(y) == 16, is_true())
+  expect_that(length(anomaly(y, n==2, method="hdr", plot=FALSE)) = 2, is_true()) 
 })
